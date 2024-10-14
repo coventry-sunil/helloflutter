edf76e9325fff09964c33898c3429d3f9d7cd0a1
Sunil Shrestha <shresth90@uni.coventry.ac.uk>
2024-10-14 23:34:27 +0545

    added new assets folder with images and local fonts folder container

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,86 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(Object context) {
     return MaterialApp(
+        theme: ThemeData(fontFamily: "Poppins"),
         home: Scaffold(
             appBar: AppBar(
               backgroundColor: Colors.blue[900],
               centerTitle: true,
-              title: const Text("Hello Flutter App!!"),
+              title: const Text("Hello Flutter"),
+              titleTextStyle: const TextStyle(
+                  color: Colors.white, fontFamily: "Poppins", fontSize: 16),
             ),
             body: Container(
-              child: Text("Hello Flutter"),
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                  color: Colors.red, borderRadius: BorderRadius.circular(10)),
+              padding: const EdgeInsets.all(20),
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(color: Colors.blue[100]),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Welcome to Hello Flutter App!",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic),
+                  ),
+                  Column(
+                    children: [
+                      Image.asset(
+                        "assets/images/flutter_icon.png",
+                        width: 100,
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        "This app is developed by Sunil!",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  Container(
+                      padding: const EdgeInsets.all(20),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/lufy.png',
+                            width: 50,
+                          ),
+                          const SizedBox(width: 20),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Sunil",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white)),
+                              Text(
+                                "Flutter Developer",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Text(
+                                "www.suneel.com.np",
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          )
+                        ],
+                      ))
+                ],
+              ),
             )));
   }
 }
